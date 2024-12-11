15a940240ee3bffe025b969b1f0780879853fac0
mahamudul hasan seber <sebermh@gmail.com>
2024-12-12 03:05:43 +0600

    add code

--- index.js
+++ index.js
@@ -39,6 +39,13 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         });
+
+        app.get('/job/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new Object(id) }
+            const result = await jobsCollection.findOne(query);
+            res.send(result);
+        });
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
